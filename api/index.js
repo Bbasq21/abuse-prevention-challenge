357836e723dfd62342a825f20ee952d8942d1a77
Freud Alexandro Munera Gonzalez <35087565+Bbasq21@users.noreply.github.com>
2026-01-22 21:51:14 -0500

    Fix: Corregir duplicacion en api/index.js

--- api/index.js
+++ api/index.js
@@ -5,11 +5,4 @@
 const app = require('../server/dist/index.js');
 
 // Exportamos la app para que Vercel la ejecute como Serverless Function
-module.exports = app.default || app;// api/index.js
-// Este archivo conecta Vercel con tu servidor Express compilado
-
-// Importamos la app compilada desde la carpeta server/dist
-const app = require('../server/dist/index.js');
-
-// Exportamos la app para que Vercel la ejecute como Serverless Function
 module.exports = app.default || app;
